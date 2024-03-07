36275b23175d40ea2cd9351fdd4c5449ef461d06
SayakSarkar02 <sayaksarkar73@gmail.com>
2024-03-07 22:29:12 +0530

    hello

--- app/page.js
+++ app/page.js
@@ -6,17 +6,17 @@ import {useEffect, useState} from "react";
 
 export default function Home() {
 
-  const [message, setMessage] = useState([]);
+  const [message, setMessage] = useState(["hiii", "hello", "how are you", "I am fine","hiii", "hello", "how are you", "I am fine", "what about you", "I am also fine"]);
   const [input, setInput ] = useState("");
 
-  const wsServer = new WebSocket('ws://localhost:4000');
+  // const wsServer = new WebSocket('ws://localhost:4000');
   
-  useEffect(() => {
-    wsServer.onmessage = (event) => {
-      console.log("jushsdhd", event.data);
-      setMessage((prev) => [...prev, `${event.data} from Server`]);
-    }
-  }, []);
+  // useEffect(() => {
+  //   wsServer.onmessage = (event) => {
+  //     console.log("jushsdhd", event.data);
+  //     setMessage((prev) => [...prev, `${event.data} from Server`]);
+  //   }
+  // }, []);
 
   const sendMessage = () => {
     console.log("hh");
@@ -24,9 +24,13 @@ export default function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-        <h1>({message})</h1>
-        <button onClick={sendMessage}>Send Message</button>
+    <main className="flex min-h-screen flex-col bg-gradient-to-br from-slate-900 to-slate-800 text-white items-center justify-center">
+        <div className="flex flex-col gap-2 text-white bg-black/40 rounded-xl w-[60%] h-80 mb-10 overflow-scroll p-5">
+          {message?.map((msg)=>(
+            <h2 className="text-white bg-white/5 px-4 py-1 w-fit rounded-md">{msg}</h2>
+          ))}
+        </div>
+        <button onClick={sendMessage} className="bg-white/5 rounded-lg px-5 py-2 hover:bg-white/10">Send Message</button>
     </main>
   );
 }
