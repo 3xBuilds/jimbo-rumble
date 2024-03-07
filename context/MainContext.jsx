cf07ac8b9cfaf44099ab0e3a207f6bb2b0367585
SayakSarkar02 <sayaksarkar73@gmail.com>
2024-03-07 20:03:56 +0530

    initial commit

---
+++ context/MainContext.jsx
@@ -0,0 +1,22 @@
+"use client";
+
+import {
+  createContext,
+  useContext,
+  useState
+} from "react";
+
+const GlobalContext = createContext();
+
+
+export const GlobalContextProvider = ({ children }) => {
+  const [loader, setLoader] = useState(false);
+
+  return (
+    <GlobalContext.Provider value={{ loader, setLoader}}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => useContext(GlobalContext);
