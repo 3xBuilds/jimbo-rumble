e3b3935b46dd5942cd29d2cdc20b488ab710ff1e
im-still-thinking <royaritra1990rocks@gmail.com>
2024-03-07 22:19:50 +0530

    socket

--- app/page.js
+++ app/page.js
@@ -1,31 +1,31 @@
 "use client"
-import {useEffect} from "react";
-import {io} from "socket.io-client";
+
+import {useEffect, useState} from "react";
+// import {io} from "socket.io-client";
 
 
 export default function Home() {
+
+  const [message, setMessage] = useState([]);
+  const [input, setInput ] = useState("");
+
+  const wsServer = new WebSocket('ws://localhost:4000');
   
   useEffect(() => {
-    const socket = io('http://localhost:3001');
-    
-    socket.on('message', (msg) => {
-        console.log('Received message:', msg);
-    });
-
-    socket.emit('message', 'Hello from the client!');
-
-    return () => {
-        socket.disconnect();
+    wsServer.onmessage = (event) => {
+      console.log("jushsdhd", event.data);
+      setMessage((prev) => [...prev, `${event.data} from Server`]);
     }
   }, []);
 
   const sendMessage = () => {
     console.log("hh");
-    socket.emit('message', 'Hello from the client!');
+    wsServer.send('Hello from the client!');
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex min-h-screen flex-col items-center justify-center">
+        <h1>({message})</h1>
         <button onClick={sendMessage}>Send Message</button>
     </main>
   );
