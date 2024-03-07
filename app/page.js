8b8d1820521b5968cd6ee60921370d353412a8e1
SayakSarkar02 <sayaksarkar73@gmail.com>
2024-03-07 21:06:59 +0530

    socket

--- app/page.js
+++ app/page.js
@@ -1,9 +1,32 @@
-import Image from "next/image";
+"use client"
+import {useEffect} from "react";
+import {io} from "socket.io-client";
+
 
 export default function Home() {
+  
+  useEffect(() => {
+    const socket = io('http://localhost:3001');
+    
+    socket.on('message', (msg) => {
+        console.log('Received message:', msg);
+    });
+
+    socket.emit('message', 'Hello from the client!');
+
+    return () => {
+        socket.disconnect();
+    }
+  }, []);
+
+  const sendMessage = () => {
+    console.log("hh");
+    socket.emit('message', 'Hello from the client!');
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-        
+        <button onClick={sendMessage}>Send Message</button>
     </main>
   );
 }
