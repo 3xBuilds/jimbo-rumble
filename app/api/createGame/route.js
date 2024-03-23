6070fb63cb02f876d3ca7562510533a53a0c8a78
Risav03 <needleweed03@gmail.com>
2024-03-23 22:47:29 +0530

    create Game

---
+++ app/api/createGame/route.js
@@ -0,0 +1,30 @@
+import Game from "@/schemas/GameSchema";
+import { connectToDB } from "@/utils/db";
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+    try{
+        await connectToDB();
+        
+        const body = await req.json();
+        const {fee, revivalFee, battleStartTime, regClosingTime, reviveLimit} = body;
+
+        const dateExists = Game.findOne(battleStartTime);
+
+        if(dateExists != null){
+            return new NextResponse(JSON.stringify({success: false, error: "Game already scheduled"}), { status: 500 });
+        }
+        else{
+            const game = await Game.create({
+                fee, revivalFee, battleStartTime, regClosingTime, reviveLimit
+            })
+            return new NextResponse(JSON.stringify({success: true, message: "Successfully Created Game", game: game}), { status: 200 });
+        }
+
+    }
+    catch (error) {
+        return new NextResponse(JSON.stringify(error), {
+            status: 500,
+        });
+    }
+}
