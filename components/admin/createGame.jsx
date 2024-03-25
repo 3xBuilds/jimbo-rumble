81ce57952de931d9feab542a676366217f539e04
SayakSarkar02 <sayaksarkar73@gmail.com>
2024-03-25 20:23:50 +0530

    createGameToggle

--- components/admin/createGame.jsx
+++ components/admin/createGame.jsx
@@ -8,8 +8,9 @@ import {useState } from 'react'
 
 export const CreateGame = () => {
 
-
     const [gameFields, setGameFields] = useState({fee: 0, revivalFee: 0, battleStartTime: 0, regCloseTime: 0, reviveLimit: 0});
+    
+    const [isOpen, setIsOpen] = useState(false);
 
     async function createGame(){
         try{
@@ -21,39 +22,43 @@ export const CreateGame = () => {
         }
     }
 
-
+ if(!isOpen) return (
+    <button onClick={()=>{setIsOpen(prev=>!prev)}} className=' bg-jimbo-green/60 px-4 py-2 text-xl rounded-xl hover:bg-jimbo-green/90 mt-5 duration-200'>Create Game</button>
+ )
+ else
 
   return (
-    <div className='bg-black/40 px-6 py-4 rounded-xl mt-10 flex items-center flex-col'>
+    <div className='bg-black/40 px-6 py-4 rounded-xl mt-5 flex items-center flex-col relative'>
+        <div onClick={()=>{setIsOpen(false)}} className='absolute text-sm top-1 right-3 cursor-pointer hover:text-jimbo-green'>x</div>
 
-<form className=''> 
-    <div className='flex-col flex'>
-        <label>Fee: </label>
-        <input onChange={(e)=>{setGameFields((prev)=>({...prev , fee: e.target.value}))}}  placeholder='0.1 SOL' className='bg-black/30 px-2 py-2 placeholder:text-jimbo-green/30 outline-jimbo-green border-2 border-black/50 rounded-xl' type='number' min={0}></input>
-    </div>
+    <form className='flex flex-col gap-3 mt-5'> 
+        <div className='flex-col flex'>
+            <label className='text-xs mb-1 text-jimbo-green'>Fee: </label>
+            <input onChange={(e)=>{setGameFields((prev)=>({...prev , fee: e.target.value}))}}  placeholder='0.1 SOL' className='bg-black/30 px-2 py-2 placeholder:text-jimbo-green/30 outline-jimbo-green border-2 border-black/50 rounded-xl' type='number' min={0}></input>
+        </div>
 
-    <div className='flex-col flex'>
-        <label>Revival Fee: </label>
-        <input onChange={(e)=>{setGameFields((prev)=>({...prev , revivalFee: e.target.value}))}}  placeholder='0.05 SOL' className='bg-black/30 px-2 py-2 placeholder:text-jimbo-green/30 outline-jimbo-green border-2 border-black/50 rounded-xl' type='number' min={0}></input>
-    </div>
+        <div className='flex-col flex'>
+            <label className='text-xs mb-1 text-jimbo-green'>Revival Fee: </label>
+            <input onChange={(e)=>{setGameFields((prev)=>({...prev , revivalFee: e.target.value}))}}  placeholder='0.05 SOL' className='bg-black/30 px-2 py-2 placeholder:text-jimbo-green/30 outline-jimbo-green border-2 border-black/50 rounded-xl' type='number' min={0}></input>
+        </div>
 
-    <div className='flex-col flex'>
-        <label>Revival Limit: </label>
-        <input onChange={(e)=>{ setGameFields((prev)=>({...prev , reviveLimit: e.target.value}))}}  placeholder='3' className='bg-black/30 px-2 py-2 placeholder:text-jimbo-green/30 outline-jimbo-green border-2 border-black/50 rounded-xl' type='number' min={0}></input>
-    </div>
+        <div className='flex-col flex'>
+            <label className='text-xs mb-1 text-jimbo-green'>Revival Limit: </label>
+            <input onChange={(e)=>{ setGameFields((prev)=>({...prev , reviveLimit: e.target.value}))}}  placeholder='3' className='bg-black/30 px-2 py-2 placeholder:text-jimbo-green/30 outline-jimbo-green border-2 border-black/50 rounded-xl' type='number' min={0}></input>
+        </div>
 
-    <div className='flex-col flex'>
-        <label>Start Date: </label>
-        <input onChange={(e)=>{setGameFields((prev)=>({...prev , battleStartTime: Date.parse(e.target.value)}))}}  placeholder='1 SOL' className='bg-white/30 px-2 py-2 placeholder:text-jimbo-green/30 outline-jimbo-green border-2 border-black/50 rounded-xl' type='datetime-local'></input>
-    </div>
+        <div className='flex-col flex'>
+            <label className='text-xs mb-1 text-jimbo-green'>Start Date: </label>
+            <input onChange={(e)=>{setGameFields((prev)=>({...prev , battleStartTime: Date.parse(e.target.value)}))}}  placeholder='1 SOL' className='bg-white/30 px-2 py-2 placeholder:text-jimbo-green/30 outline-jimbo-green border-2 border-black/50 rounded-xl' type='datetime-local'></input>
+        </div>
 
-    <div className='flex-col flex'>
-        <label>Regs Closing Date: </label>
-        <input onChange={(e)=>{setGameFields((prev)=>({...prev , regCloseTime: Date.parse(e.target.value)}))}}  placeholder='1 SOL' className='bg-white/30 px-2 py-2 placeholder:text-jimbo-green/30 outline-jimbo-green border-2 border-black/50 rounded-xl' type='datetime-local'></input>
-    </div>
-</form>
+        <div className='flex-col flex'>
+            <label className='text-xs mb-1 text-jimbo-green'>Regs Closing Date: </label>
+            <input onChange={(e)=>{setGameFields((prev)=>({...prev , regCloseTime: Date.parse(e.target.value)}))}}  placeholder='1 SOL' className='bg-white/30 px-2 py-2 placeholder:text-jimbo-green/30 outline-jimbo-green border-2 border-black/50 rounded-xl' type='datetime-local'></input>
+        </div>
+    </form>
 
-<button onClick={createGame} className=' bg-jimbo-green/60 px-4 py-2 text-xl rounded-xl hover:bg-jimbo-green/90 mt-5 duration-200'>Create Game</button>
+<button onClick={createGame} className=' bg-jimbo-green/60 px-4 py-2 text-lg rounded-xl hover:bg-jimbo-green/90 mt-5 duration-200'>Create Game</button>
     </div>
   )
 }
